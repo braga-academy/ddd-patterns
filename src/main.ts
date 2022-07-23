a049a39a78242ac92cf1b6326058c40735c3ae87
lucianobragaweb <lucianobragaweb@gmail.com>
2022-07-23 16:53:25 -0300

    working with agregates

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import Address from './entity/address'
+import Customer from './entity/customer'
+import Order from './entity/order'
+import OrderItem from './entity/order_item'
+
+const customer = new Customer("123", "Luciano Braga")
+
+const address = new Address('Rua teste', 2, '62190000', 'Groaíras')
+
+customer.Address = address
+customer.activate()
+
+const item1 = new OrderItem('1', 'Item Teste 1', 10)
+const item2 = new OrderItem('2', 'Item Teste 2', 20)
+
+const order = new Order('1', customer._id, [item1, item2])
