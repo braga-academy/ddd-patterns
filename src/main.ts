f773f976afd6c9876083ee7c6fc6b57ddfe3d5ee
lucianobragaweb <lucianobragaweb@gmail.com>
2022-07-25 19:46:18 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,4 @@ customer.activate()
 const item1 = new OrderItem('o1', 'p1', 2, 'Item Teste 1', 10)
 const item2 = new OrderItem('o2', 'p2', 3, 'Item Teste 2', 20)
 
-const order = new Order('1', customer._id, [item1, item2])
+const order = new Order('1', customer.id, [item1, item2])
